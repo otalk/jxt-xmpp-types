b2279911492b7ccbb3841137e135c79cfb9e480a
Lance Stout <lancestout@gmail.com>
2014-09-21 17:11:36 -0700

    Work as a JXT plugin

--- index.js
+++ index.js
@@ -1,75 +1,120 @@
 'use strict';
 
-var jxt = require('jxt');
 var JID = require('xmpp-jid').JID;
 
 
-exports.jidAttribute = function (attr, prepped) {
-    return {
-        get: function () {
-            var jid = new JID(jxt.getAttribute(this.xml, attr));
-            if (prepped) {
-                jid.prepped = true;
-            }
-            return jid;
-        },
-        set: function (value) {
-            jxt.setAttribute(this.xml, attr, (value || '').toString());
-        }
+module.exports = function (JXT) {
+    var types = JXT.utils;
+
+    // ----------------------------------------------------------------
+    // Shortcuts for common withDefinition calls
+    // ----------------------------------------------------------------
+
+    JXT.withIq = function (cb) {
+        this.withDefinition('iq', 'jabber:client', cb);
+        this.withDefinition('iq', 'jabber:component:accept', cb);
     };
-};
 
-
-exports.jidSub = function (NS, sub, prepped) {
-    return {
-        get: function () {
-            var jid = new JID(jxt.getSubText(this.xml, NS, sub));
-            if (prepped) {
-                jid.prepped = true;
-            }
-            return jid;
-        },
-        set: function (value) {
-            jxt.setSubText(this.xml, NS, sub, (value || '').toString());
-        }
+    JXT.withMessage = function (cb) {
+        this.withDefinition('message', 'jabber:client', cb);
+        this.withDefinition('message', 'jabber:component:accept', cb);
     };
-};
+
+    JXT.withPresence = function (cb) {
+        this.withDefinition('presence', 'jabber:client', cb);
+        this.withDefinition('presence', 'jabber:component:accept', cb);
+    };
+
+    JXT.withStreamFeatures = function (cb) {
+        this.withDefinition('features', 'http://etherx.jabber.org/streams', cb);
+    };
+
+    JXT.withStanzaError = function (cb) {
+        this.withDefinition('error', 'jabber:client', cb);
+        this.withDefinition('error', 'jabber:component:accept', cb);
+    };
+
+    JXT.withDataForm = function (cb) {
+        this.withDefinition('x', 'jabber:x:data', cb);
+    };
+
+    JXT.withPubsubItem = function (cb) {
+        this.withDefinition('item', 'http://jabber.org/protocol/pubsub', cb);
+        this.withDefinition('item', 'http://jabber.org/protocol/pubsub#event', cb);
+    };
 
 
-exports.tzoSub = jxt.field(
-    function (xml, NS, sub, defaultVal) {
-        var split, hrs, min;
-        var sign = -1;
-        var formatted = jxt.getSubText(xml, NS, sub);
+    // ----------------------------------------------------------------
+    // Field types
+    // ----------------------------------------------------------------
 
-        if (!formatted) {
-            return defaultVal;
-        }
 
-        if (formatted.charAt(0) === '-') {
-            sign = 1;
-            formatted = formatted.slice(1);
-        }
-
-        split = formatted.split(':');
-        hrs = parseInt(split[0], 10);
-        min = parseInt(split[1], 10);
-        return (hrs * 60 + min) * sign;
-    },
-    function (xml, NS, sub, value) {
-        var hrs, min;
-        var formatted = '-';
-        if (typeof value === 'number') {
-            if (value < 0) {
-                value = -value;
-                formatted = '+';
+    JXT.utils.jidAttribute = function (attr, prepped) {
+        return {
+            get: function () {
+                var jid = new JID(types.getAttribute(this.xml, attr));
+                if (prepped) {
+                    jid.prepped = true;
+                }
+                return jid;
+            },
+            set: function (value) {
+                types.setAttribute(this.xml, attr, (value || '').toString());
             }
-            hrs = value / 60;
-            min = value % 60;
-            formatted += (hrs < 10 ? '0' : '') + hrs + ':' + (min < 10 ? '0' : '') + min;
-        } else {
-            formatted = value;
+        };
+    };
+    
+    JXT.utils.jidSub = function (NS, sub, prepped) {
+        return {
+            get: function () {
+                var jid = new JID(types.getSubText(this.xml, NS, sub));
+                if (prepped) {
+                    jid.prepped = true;
+                }
+                return jid;
+            },
+            set: function (value) {
+                types.setSubText(this.xml, NS, sub, (value || '').toString());
+            }
+        };
+    };
+    
+    
+    JXT.utils.tzoSub = types.field(
+        function (xml, NS, sub, defaultVal) {
+            var split, hrs, min;
+            var sign = -1;
+            var formatted = types.getSubText(xml, NS, sub);
+    
+            if (!formatted) {
+                return defaultVal;
+            }
+    
+            if (formatted.charAt(0) === '-') {
+                sign = 1;
+                formatted = formatted.slice(1);
+            }
+    
+            split = formatted.split(':');
+            hrs = parseInt(split[0], 10);
+            min = parseInt(split[1], 10);
+            return (hrs * 60 + min) * sign;
+        },
+        function (xml, NS, sub, value) {
+            var hrs, min;
+            var formatted = '-';
+            if (typeof value === 'number') {
+                if (value < 0) {
+                    value = -value;
+                    formatted = '+';
+                }
+                hrs = value / 60;
+                min = value % 60;
+                formatted += (hrs < 10 ? '0' : '') + hrs + ':' + (min < 10 ? '0' : '') + min;
+            } else {
+                formatted = value;
+            }
+            types.setSubText(xml, NS, sub, formatted);
         }
-        jxt.setSubText(xml, NS, sub, formatted);
-    }
-);
+    );
+};
